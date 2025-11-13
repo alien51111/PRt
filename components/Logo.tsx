93449049d75a8ed25fee186d07a6ac7f096510e4
Noor Banana <alien@Noors-MacBook-Air.local>
2025-11-13 12:18:19 +0300

    Added new logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -3,10 +3,10 @@ import React from 'react';
 const Logo: React.FC = () => {
   return (
     <img 
-      src="https://via.placeholder.com/78x36/CBFF08/0B5351?text=PRt" 
-      alt="PRt Agency Logo"
-      width="78"
-      height="36"
+    img src="/logo.png" 
+    alt="PRt Logo" 
+      width="75"
+      height="33"
     />
   );
 };
